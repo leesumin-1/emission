676fc874e50670c37a2967b93e25318783b03b7b
unknown <sally0680@naver.com>
2021-05-18 01:57:36 +0900

    modified file

--- emission.py
+++ emission.py
@@ -1,6 +1,8 @@
 import time
 import selenium
 from selenium import webdriver
+import matplotlib.pyplot as plt
+import matplotlib
 
 URL = 'http://www.index.go.kr/potal/main/EachDtlPageDetail.do?idx_cd=1464&param=013'
 
@@ -8,6 +10,11 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 
 driver = webdriver.Chrome(executable_path='./chromedriver.exe', options=options)
+
+# 리스트
+year_list = []
+total_emission_list = []
+a = []
 
 #연도 출력
 driver.get(url=URL)
@@ -23,8 +30,10 @@ year_table_tbody_tr_list=year_table_tbody[0].find_elements_by_tag_name("tr")
 for year_table_tbody_tr in year_table_tbody_tr_list :
     year_table_tbody_tr_td_list=year_table_tbody_tr.find_elements_by_tag_name("th")
     for year_table_tbody_tr_td in year_table_tbody_tr_td_list :
-        print("\t", year_table_tbody_tr_td.text ,end="")
-    print("")
+        a.append(year_table_tbody_tr_td.text)
+        #print("\t", year_table_tbody_tr_td.text ,end="")
+        #print(a)
+    #print("")
 time.sleep(3)
 
 # 온실가스 배출량 출력
@@ -38,12 +47,27 @@ emission_table_tbody=emission_table[0].find_elements_by_tag_name("tbody")
 emission_table_tbody_tr_list=emission_table_tbody[0].find_elements_by_tag_name("tr")
 # print(len(weather_table_tbody_tr_list))
 
-emission_list = []
 for emission_table_tbody_tr in emission_table_tbody_tr_list :
     emission_table_tbody_tr_td_list=emission_table_tbody_tr.find_elements_by_tag_name("td")
     for emission_table_tbody_tr_td in emission_table_tbody_tr_td_list :
-        print("\t", emission_table_tbody_tr_td.text, end="")
+        total_emission_list.append(emission_table_tbody_tr_td.text)
+        #print("\t", emission_table_tbody_tr_td.text ,end=" ")
+        #print(total_emission_list)
+        break
     print("")
 time.sleep(3)
 
 driver.close()
+
+for i in range(1,9):
+    year_list.append(a[i])
+
+print(year_list)
+print(total_emission_list)
+
+
+#그래프 만들기
+matplotlib.rcParams["axes.unicode_minus"]=False #폰트 깨짐 대처
+plt.rc('font', family='Malgun Gothic')
+x=len(year_list)
+plt.bar(range(x), total_emission_list)
